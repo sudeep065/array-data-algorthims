2ab7c471c167c461f641d6fb2e33acaa8067a3da
Sudi065 <sudeep065@users.noreply.github.com>
2021-02-18 21:59:59 -0500

    calculate the absolute difference between the sums of its diagonals

--- Program.cs
+++ Program.cs
@@ -29,5 +29,20 @@ namespace array_data_algorthims
             }
             return result;
         }
+
+        // Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+        public static int diagonalDifference(List<List<int>> arr)
+        {
+            int diag1Sum = 0; //TL > BR
+            int diag2Sum = 0; //TR > BL
+            int col = 0;
+            for (int row = 0; row < arr.Count; row++)
+            {
+                diag1Sum += arr[row][col]; //[2][2] = -12
+                diag2Sum += arr[row][arr[row].Count - 1 - col]; //[2][3 - 1 - 2]= [0] == 10
+                col++;
+            }
+            return Math.Abs(diag1Sum - diag2Sum);
+        }
     }
 }
